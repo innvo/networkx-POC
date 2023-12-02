f73875c61267f40e235feccd0b4e261da7f3bc5e
Eric Chasin <echasin@innvosolutions.com>
2023-12-02 11:36:33 -0800

    Added border, subtitle and legend

--- networkxGraphExample-1.py
+++ networkxGraphExample-1.py
@@ -43,13 +43,41 @@ edge_colors = [edge_color_map[G.edges[edge]['edge_type']] for edge in G.edges]
 # Create an edge labels dictionary
 edge_labels = {(u, v): G.edges[u, v]['edge_type'] for u, v in G.edges}
 
+# Set the figure size
+plt.figure(figsize=(10, 10), dpi=80)
+
+# Adjust the top of the subplot parameters
+plt.subplots_adjust(top=2.0)
+
+# Create a node size list
+node_size = [10000 / len(G.nodes) for _ in G.nodes]
+print(node_size)
+
 # Plot the graph
-nx.draw(G, pos, node_color=colors, edge_color=edge_colors, labels=labels, with_labels=True)
+nx.draw(G, pos, node_color=colors, edge_color=edge_colors, node_size=node_size,labels=labels, with_labels=True)
 nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
 
 # Create a legend
-legend_handles = [mpatches.Patch(color=color, label=node_type) for node_type, color in color_map.items()]
-plt.legend(handles=legend_handles)
+node_legend_handles = [mpatches.Patch(color=color, label=node_type) for node_type, color in color_map.items()]
+edge_legend_handles = [mpatches.Patch(color=color, label=edge_type) for edge_type, color in edge_color_map.items()]
+
+plt.legend(handles=node_legend_handles + edge_legend_handles)
+
+# Add a title (acting as subtitle)
+plt.title('Subtitle Here') ## THIS IS NOT WORKING
+
+# Add a suptitle (acting as main title)
+plt.suptitle('Example Network Graph', weight='bold', fontsize='16')
+# Get the current axes
+ax = plt.gca()
+
+# Get the range of the axes
+xlim = ax.get_xlim()
+ylim = ax.get_ylim()
+
+# Add a border
+plt.plot([xlim[0], xlim[0], xlim[1], xlim[1], xlim[0]], [ylim[0], ylim[1], ylim[1], ylim[0], ylim[0]], color='black')
+
 
 plt.show()
 
